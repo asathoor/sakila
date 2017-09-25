32560d139e2953cb7b32573f45c2b16a31b509d4
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2017-09-25 16:45:52 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,16 +1,20 @@
 <?php
-// CONNECT TO THE SAKILA DATABASE
+/** 
+ * file: db.php 
+ * purpose: connector
+*/
 $host = "localhost";
 $password = "password";
 $user = "root";
 $database = "sakila";
 
+// connect
 $mysqli = new mysqli( $host, $user, $password, $database ); // creates the object
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error; // if error messages
 }
 
-/* test your connection */
+// test 
 echo "Your're connected to the database via: " 
 . $mysqli->host_info 
 . "\n";
