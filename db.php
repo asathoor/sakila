09fd0acef8ba96c7442ce0188aba34cc4064c4d1
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2017-09-25 16:47:29 +0200

    Update db.php

--- db.php
+++ db.php
@@ -10,12 +10,15 @@ $database = "sakila";
 
 // connect
 $mysqli = new mysqli( $host, $user, $password, $database ); // creates the object
+
+// if error
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error; // if error messages
 }
-
-// test 
-echo "Your're connected to the database via: " 
-. $mysqli->host_info 
-. "\n";
+else {
+    // if connected
+    echo "Your're connected to the database via: " 
+    . $mysqli->host_info 
+    . "\n";
+}
 ?>
