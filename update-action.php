8d091e59eebc75911003496d40b6d60a9f238353
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2017-10-03 13:12:11 +0200

    Update update-action.php

--- update-action.php
+++ update-action.php
@@ -15,42 +15,10 @@ $sql = stripslashes($sql);
 /* database connection */
 require "db.php";
 
-/* execute the query */
-
-/* Test ----------
-
-if($mysqli->query($sql)){
-	echo "<p>Database updated</p>";
-} else {
-	echo "<p>Damn, something went wrong here!</p>";
-}
-------------- */
-
-/* edit string from PhpMyAdmin */
-
-/*
-The form looked like this:
-	...
-	<td><input type='text' name='actorFirstName' value='<?php echo $firstName; ?>'></td>
-	<td><input type='text' name='actorLastName' value='<?php echo $lastName; ?>'></td>
-	...
-	<td><input type='hidden' name='id' value='<?php echo $actorId; ?>'></td>
-	...
-*/
-
 $firstName = $_REQUEST['actorFirstName'];
 $lastName = $_REQUEST['actorLastName'];
 $id = $_REQUEST['id'];
 
-/* IMPORTANT NOTE */
-
-// 
-// add slashes, remove tags etc. here
-//
-
-
-/* SQL string with input from the form */
-$sql = "UPDATE `sakila`.`actor` SET `first_name` = '". $firstName ."', `last_name` = '$lastName'  WHERE `actor`.`actor_id` = $id;";
 ?>
 
 <p>SQL sentence: <?php echo $sql; ?></p>
